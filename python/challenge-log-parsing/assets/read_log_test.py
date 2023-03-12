b3c8436778b72dc251a66430c30f80e730486845
zeying <redyhaikuotiankong@qq.com>
2023-03-12 16:05:41 +0800

    add challenge test log

--- python/challenge-log-parsing/assets/read_log_test.py
+++ python/challenge-log-parsing/assets/read_log_test.py
@@ -8,11 +8,14 @@ from read_log import read_log
 
 class TestReadLog(unittest.TestCase):
     def test_read_log(self):
-        file_name = 'x.log'
-        self.assertRaises(read_log(file_name), FileNotFoundError)
+        try:
+            file_name = 'x.log'
+            read_log(file_name)
+        except Exception:
+            self.assertRaises(FileNotFoundError)
 
         file_name = 'access.log'
-        self.assertEqual(read_log(file_name), 1997)
+        self.assertEqual(read_log(file_name), 999)
 
 
 if __name__ == "__main__":
