5de7b9f0c37d5fab5852b6f1babe57ec72f6cab4
zeying <redyhaikuotiankong@qq.com>
2023-03-20 22:36:50 +0800

    add docs here

--- python/challenge-area-calculation/assets/shape.py
+++ python/challenge-area-calculation/assets/shape.py
@@ -1,5 +1,8 @@
 class Shape:
     """
+    Initialize this class, create init method and area() method. 
+    """
+    """
     TODO: implement this class
     Note: Do not change the existing code
     """
--- python/challenge-area-calculation/assets/subclass.py
+++ python/challenge-area-calculation/assets/subclass.py
@@ -1,10 +1,16 @@
 class Circle:
     """
+    Initialize Circle here. 
+    """
+    """
     TODO: Write your code here
     """
 
 
 class Square:
+    """
+    Initialize Square here. 
+    """
     """
     TODO: Write your code here
     """
