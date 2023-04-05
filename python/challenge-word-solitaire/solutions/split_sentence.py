1d949f0f6f081a686892107d3df2df1007e31083
zeying <redyhaikuotiankong@qq.com>
2023-04-05 08:50:14 +0800

    remove Chinese comment

--- python/challenge-word-solitaire/solutions/split_sentence.py
+++ python/challenge-word-solitaire/solutions/split_sentence.py
@@ -1,9 +1,7 @@
 import re
 
 def process_sentence(sentence):
-    # 去除标点符号、数字及包含字母以外的单词，将所有字母转为小写，并分割为单词列表
     result = re.findall(r'\b[a-zA-Z]+\b', sentence.lower())
-    # 去除重复项，相同的单词仅保留一个
     result = list(set(result))
     return result
 
