84248ebe8aa70389ca9ca23f91a29b7044d3c337
zeying <redyhaikuotiankong@qq.com>
2023-04-13 21:33:46 +0800

    format solution code

--- python/challenge-log-parsing/solutions/read_log.py
+++ python/challenge-log-parsing/solutions/read_log.py
@@ -1,6 +1,7 @@
 
-def read_log(file_name):
+def read_log(file_name: str) -> int:
     line_count = 0
+    
     with open(file_name, "r") as f:
         for line in f.readlines():
             print(line)
